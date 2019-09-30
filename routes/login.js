8ff3aa4e62ddae51f1b0fcc25d1c49b11aaf378c
KaushalVAmancherla <amancherlakaushal@gmail.com>
2019-09-29 23:35:08 -0700

    Test Commit

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET login page. */
 router.get('/', function(req, res, next) {
-    res.render('login', { title: 'Login'});
+    res.render('login', { title: 'NewInternetPlatform Login'});
 });
 
 module.exports = router;
