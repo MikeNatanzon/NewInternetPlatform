0cbd893c9c2dac72c6565db5f66be681a1ea9ff3
Alessandro Santamaro <alex88na@hotmail.it>
2019-12-17 14:18:35 +0100

    Added new response structure.

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,30 +7,45 @@ const errorHandler = require('http-errors');
 
 //POST new user route (optional, everyone has access)
 router.post('/', auth.optional, (req, res, next) => {
-    const { body: { user } } = req;
+    const {body: {user}} = req;
 
-    if(!user) {
-        return res.status(422).json({
-            errors: {
-                user: 'data are required',
-            },
-        });
+    if (!user) {
+        return res
+            .status(422)
+            .json({
+                data: null,
+                errors: {
+                    message: 'User data are required',
+                },
+                info: null,
+                warnings: null
+            });
     }
 
-    if(!user.email) {
-        return res.status(422).json({
-            errors: {
-                email: 'is required',
-            },
-        });
+    if (!user.email) {
+        return res
+            .status(422)
+            .json({
+                data: null,
+                errors: {
+                    message: 'Email is required',
+                },
+                info: null,
+                warnings: null
+            });
     }
 
-    if(!user.password) {
-        return res.status(422).json({
-            errors: {
-                password: 'is required',
-            },
-        });
+    if (!user.password) {
+        return res
+            .status(422)
+            .json({
+                data: null,
+                errors: {
+                    message: 'Password is required',
+                },
+                info: null,
+                warnings: null
+            });
     }
 
     const finalUser = new Users(user);
@@ -38,51 +53,77 @@ router.post('/', auth.optional, (req, res, next) => {
     finalUser.setPassword(user.password);
 
     return finalUser.save()
-        .then(() => res.json({ user: finalUser.toAuthJSON() }));
+        .then(() => {
+            return res
+                .status(200)
+                .json({
+                    data: {
+                        user: finalUser.toAuthJSON()
+                    },
+                    errors: null,
+                    info: null,
+                    warnings: null
+                });
+        });
 });
 
 //POST login route (optional, everyone has access)
 router.post('/login', auth.optional, (req, res, next) => {
-    let { body: { user } } = req;
+    let {body: {user}} = req;
 
-    if(!user) {
+    if (!user) {
         if (req.body && req.body.email && req.body.password) {
             user = {
-                email: req.body.email ,
+                email: req.body.email,
                 password: req.body.password
             };
             req.body = {user: user};
         } else {
-            return res.status(422).json({
-                errors: {
-                    user: 'data are required',
-                },
-            });
+            return res
+                .status(422)
+                .json({
+                    data: null,
+                    errors: {
+                        message: 'User data are required'
+                    },
+                    info: null,
+                    warnings: null
+                });
         }
     }
 
-    if(!user.email) {
-        return res.status(422).json({
-            errors: {
-                email: 'is required',
-            },
-        });
+    if (!user.email) {
+        return res
+            .status(422)
+            .json({
+                data: null,
+                errors: {
+                    message: 'Email is required'
+                },
+                info: null,
+                warnings: null
+            });
     }
 
-    if(!user.password) {
-        return res.status(422).json({
-            errors: {
-                password: 'is required',
-            },
-        });
+    if (!user.password) {
+        return res
+            .status(422)
+            .json({
+                data: null,
+                errors: {
+                    message: 'Password is required'
+                },
+                info: null,
+                warnings: null
+            });
     }
 
-    return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
-        if(err) {
+    return passport.authenticate('local', {session: false}, (err, passportUser, info) => {
+        if (err) {
             return next(err);
         }
 
-        if(passportUser) {
+        if (passportUser) {
             console.log('Logged user');
             const user = passportUser;
             user.token = passportUser.generateJWT();
@@ -90,15 +131,25 @@ router.post('/login', auth.optional, (req, res, next) => {
             return res
                 .status(200)
                 .json({
-                    user: user.toAuthJSON()
+                    data: {
+                        user: user.toAuthJSON()
+                    },
+                    errors: null,
+                    info: null,
+                    warnings: null
                 });
         }
 
         console.log('Invalid credentials');
         return res
             .status(500)
-            .send({
-                message: 'Invalid credentials'
+            .json({
+                data: null,
+                errors: {
+                    message: 'Invalid credentials',
+                },
+                info: null,
+                warnings: null
             });
 
     })(req, res, next);
@@ -106,140 +157,193 @@ router.post('/login', auth.optional, (req, res, next) => {
 
 //POST login route (optional, everyone has access)
 router.post('/signup', auth.optional, (req, res, next) => {
-    let { body: { user } } = req;
+    let {body: {user}} = req;
 
     user = {
-                email: req.body.email ,
-                password: req.body.password ,
-                password_retype: req.body.password_retype
-            };
+        email: req.body.email,
+        password: req.body.password,
+        password_retype: req.body.password_retype
+    };
 
-    if(!user.email) {
-        return res.status(422).json({
-            errors: 'Email is required'
-        });
+    if (!user.email) {
+        return res
+            .status(422)
+            .json({
+                data: null,
+                errors: {
+                    message: 'Email is required'
+                },
+                info: null,
+                warnings: null
+            });
     }
 
-    if(!user.password) {
-        return res.status(422).json({
-            errors: 'Password is required'
-        });
+    if (!user.password) {
+        return res
+            .status(422)
+            .json({
+                data: null,
+                errors: {
+                    message: 'Password is required'
+                },
+                info: null,
+                warnings: null
+            });
     }
 
-
-    if(!user.password_retype) {
-        return res.status(422).json({
-            errors: 'Password retype is required'
-       });
+    if (!user.password_retype) {
+        return res
+            .status(422)
+            .json({
+                data: null,
+                errors: {
+                    message: 'Password retype is required'
+                },
+                info: null,
+                warnings: null
+            });
     }
 
-    if(user.password_retype!==user.password) {
-        return res.status(422).json({
-            errors: 'Passwords do not match'
-        });
+    if (user.password_retype !== user.password) {
+        return res
+            .status(422)
+            .json({
+                data: null,
+                errors: {
+                    message: 'Passwords do not match'
+                },
+                info: null,
+                warnings: null
+            });
     }
 
     let strengthValue = {
-      'caps': false,
-      'length': false,
-      'special': false,
-      'numbers': false,
-      'small': false
+        'caps': false,
+        'length': false,
+        'special': false,
+        'numbers': false,
+        'small': false
     };
 
-    if(user.password.length >= 8) {
+    if (user.password.length >= 8) {
         strengthValue.length = true;
     }
 
-    for(let index=0; index < user.password.length; index++) {
-          let char = user.password.charCodeAt(index);
+    for (let index = 0; index < user.password.length; index++) {
+        let char = user.password.charCodeAt(index);
 
-          if(!strengthValue.caps && char >= 65 && char <= 90) {
-              strengthValue.caps = true;
-          } else if(!strengthValue.numbers && char >=48 && char <= 57){
+        if (!strengthValue.caps && char >= 65 && char <= 90) {
+            strengthValue.caps = true;
+        } else if (!strengthValue.numbers && char >= 48 && char <= 57) {
             strengthValue.numbers = true;
-          } else if(!strengthValue.small && char >=97 && char <= 122){
+        } else if (!strengthValue.small && char >= 97 && char <= 122) {
             strengthValue.small = true;
-          } else if(!strengthValue.numbers && char >=48 && char <= 57){
+        } else if (!strengthValue.numbers && char >= 48 && char <= 57) {
             strengthValue.numbers = true;
-          } else if(!strengthValue.special && (char >=33 && char <= 47) || (char >=58 && char <= 64)) {
+        } else if (!strengthValue.special && (char >= 33 && char <= 47) || (char >= 58 && char <= 64)) {
             strengthValue.special = true;
-          }
+        }
     }
 
-    if (!(strengthValue.caps && strengthValue.length && strengthValue.special && strengthValue.numbers && strengthValue.small))
-    {
-        return res.status(422).json({
-            errors: 'Password should contain at least 8 alpha-numeric characters with at least one uppercase, one number, and one special character'
-        });
+    if (!(strengthValue.caps && strengthValue.length && strengthValue.special && strengthValue.numbers && strengthValue.small)) {
+        return res
+            .status(422)
+            .json({
+                data: null,
+                errors: {
+                    message: 'Password should contain at least 8 alpha-numeric characters with at least one uppercase, one number, and one special character'
+                },
+                info: null,
+                warnings: null
+            });
     }
-
 
     var mongoUtil = require('../../config/mongoUtil');
     var db = mongoUtil.getDb();
 
-   //db.collection('Users').find();
+    //db.collection('Users').find();
 
     //console.log("*** " + user.email);
 
-   Users.findOne({ email: user.email })
-    .then((doc) => {
-       if (doc) {
-           // console.log("MVMVMV ~~~~~~~~~~~~~ user already exists");
+    Users.findOne({email: user.email})
+        .then((doc) => {
+            if (doc) {
+                // console.log("MVMVMV ~~~~~~~~~~~~~ user already exists");
+
+                return res
+                    .status(422)
+                    .json({
+                        data: null,
+                        errors: {
+                            message: 'User already exists'
+                        },
+                        info: null,
+                        warnings: null
+                    });
+
+            } else {
+                // console.log("MVMVMV ~~~~~~~~~~~~~ no data exist for this id");
 
 
-            return res.status(422).json({
-               errors: 'User already exists'
-            });
+                var doc = new Users({email: user.email, hash: user.password, salt: user.password});
+                doc.setPassword(user.password);
 
-       } else {
-            // console.log("MVMVMV ~~~~~~~~~~~~~ no data exist for this id");
+                // save model to database
+                doc.save(function (err, doc) {
 
+                    if (err) return console.error(err);
+                    return res
+                        .status(200)
+                        .json({
+                            data: {
+                                message: 'Congrats, you are now an esteemed NewInternet member.'
+                            },
+                            errors: null,
+                            info: null,
+                            warnings: null
+                        });
 
-            var doc = new Users({ email: user.email, hash: user.password, salt: user.password});
-            doc.setPassword(user.password);
+                });
 
-            // save model to database
-            doc.save(function (err, doc) {
-
-            if (err) return console.error(err);
-            //console.log(doc.email + " saved to users collection.");
-
-            return res.status(200).json({
-                status: 'Congrats, you are now an esteemed NewInternet member.',
-            });
-
-            });
-
-
-       }
-    })
-   .catch((err) => {
-     console.log(err);
-    });
-
-
-
-
-
-
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 
     // a document instance
 
 });
 
-
 //GET current route (required, only authenticated users have access)
 router.get('/current', auth.required, (req, res, next) => {
-    const { payload: { id } } = req;
+    const {payload: {id}} = req;
 
     return Users.findById(id)
         .then((user) => {
-            if(!user) {
-                return res.sendStatus(400);
+            if (!user) {
+                return res
+                    .status(400)
+                    .json({
+                        data: null,
+                        errors: {
+                            message: 'User not logged'
+                        },
+                        info: null,
+                        warnings: null
+                    });
             }
 
-            return res.json({ user: user.toAuthJSON() });
+            return res
+                .status(200)
+                .json({
+                    data: {
+                        user: user.toAuthJSON()
+                    },
+                    errors: null,
+                    info: null,
+                    warnings: null
+                });
         });
 });
 
